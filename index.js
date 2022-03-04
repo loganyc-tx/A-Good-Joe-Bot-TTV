c2ada11e22d398d11eaa66234962fb594b9923be
Logan C <loganyc@tamu.edu>
2022-03-04 13:07:15 -0600

    basic functionality

--- index.js
+++ index.js
@@ -14,3 +14,32 @@ const opts = {
 };
 
 // New client
+const client = new tmi.client(opts);
+
+// Event handler registration
+client.on('message', onMessageHandler);
+client.on('connected', onConnectedHandler);
+
+// Connect to ttv
+client.connect();
+
+// Called if there is a message
+function onMessageHandler (target, context, msg, self){
+    if(self){ return; } //ignore self
+
+    //Remove whitespace
+    const commandName = msg.trim().toLowerCase();
+
+    // do da command
+    if (commandName === '!help'){
+        client.say(target, 'Yeah, me too pal.');
+        console.log('* Executed command ${commandName}');
+    }else{
+        console.log('* Unknown command ${commandName}');
+    }
+}
+
+// Called on connect
+function onConnectedHandler (addr, port){
+    console.log('Successfuly connection to $(addr):$(port)');
+}
