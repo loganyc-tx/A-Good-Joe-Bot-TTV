365a11b1ee2e4824b4ef8a33c3430c6cd3b0193a
Logan C <loganyc@tamu.edu>
2022-03-04 14:38:26 -0600

    playsound test

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const tmi = require('tmi.js');
 var irc = require('irc'),
     user = require("./privateVars.json")
+const {Howl, Howler} = require('howler'); 
 
 //config options
 const opts = {
@@ -14,6 +15,14 @@ const opts = {
         user.CHANNEL_NAME
     ]
 };
+
+var STFUsound = new Howl({
+    src: ['audio/STFU.mp3']
+});
+var ANGERsound = new Howl({
+    src: ['audio/bladrunnerANGRY.mp3']
+});
+
 
 // New client
 const client = new tmi.client(opts);
@@ -31,11 +40,24 @@ function onMessageHandler (target, context, msg, self){
 
     //Remove whitespace
     const commandName = msg.trim().toLowerCase();
-
+    var args = commandName.split(' ');
+    console.log(args.toString() + ".");
     // do da command
-    if (commandName === '!help'){
+    if (args[0] === '!help'){
         client.say(target, 'Yeah, me too pal.');
-        console.log('* Executed command ${commandName}');
+        
+    }else if(args[0] === '!ps'){
+        console.log('* Executed command !ps');
+        if(args.length < 2){
+            client.say(target, 'Give me something to work with here. (STFU, AGH)');
+        }else if(args[1] === 'stfu'){
+            STFUsound.play(); 
+        }else if(args[1] === 'agh'){
+            ANGERsound.play();
+        }else{
+            client.say(target, 'Not a valid playsound. (STFU, AGH)');
+        }
+    
     }else{
         console.log('* Unknown command ${commandName}');
     }
