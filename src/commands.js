5af6831b5acc2a448ac7c47ce1456f8f8d583902
LoganYC <loganyutichen@gmail.com>
2022-03-16 01:06:20 -0500

    added the rest of the playsounds

--- src/commands.js
+++ src/commands.js
@@ -20,9 +20,23 @@ const commands = [
       handler: async (client, channel, tag, args) => {
         console.log("playsound");
         audio = document.getElementById("sound");
-        audio.setAttribute("src", "audio/bladerunnerANGRY.mp3");
-        audio.setAttribute("muted", "false");
-        audio.play();
+        if(args.length == 0){
+          client.say(channel, 'Playsounds are - kanger, stfu, sorry4what');
+        }else if(args[0].toLowerCase() == "kanger"){
+          audio.setAttribute("src", "audio/bladerunnerANGRY.mp3");
+          audio.setAttribute("muted", "false");
+          audio.play();
+        }else if(args[0].toLowerCase() == "stfu"){
+          audio.setAttribute("src", "audio/STFU.mp3");
+          audio.setAttribute("muted", "false");
+          audio.play();
+        }else if(args[0].toLowerCase() == "sorry4what"){
+          audio.setAttribute("src", "audio/Sorry for what.mp3");
+          audio.setAttribute("muted", "false");
+          audio.play();
+        }else{
+          client.say(channel, "I don't know that one. Playsounds are - kanger, stfu, sorry4what");
+        }
         
       },
     },
